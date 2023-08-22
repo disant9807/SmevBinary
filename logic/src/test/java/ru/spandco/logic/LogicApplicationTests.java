547d5542e5beb23680a6cb23dbd46d7ab68b6681
Denis <d.spicyn@gmail.com>
2023-08-22 19:24:06 +0800

    second commit

---
+++ logic/src/test/java/ru/spandco/logic/LogicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spandco.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
